ceee1e4b0fa90634450b063e357f50a4cd3c99b7
Theodore Roley <tvroley@uw.edu>
2023-11-30 00:07:53 -0800

    hello darkness clears interval when it gets to 0

--- hello-darkness.js
+++ hello-darkness.js
@@ -1,8 +1,10 @@
 const body = document.getElementsByTagName('body')[0];
 let darkness = 255;
-let countdownInterval = setInterval(function() {
+const countdownInterval = setInterval(function() {
     if(darkness > 0) {
         darkness--;
         body.style.backgroundColor = `rgb(${darkness}, ${darkness}, ${darkness})`;
+    } else {
+        clearInterval(countdownInterval);
     }
 }, 500);
