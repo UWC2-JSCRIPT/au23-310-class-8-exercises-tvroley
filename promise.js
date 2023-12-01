475492bb9aa0594c8a302e9080972e60c2d29a91
Theodore Roley <tvroley@uw.edu>
2023-11-30 16:41:02 -0800

    first attempt at promise

--- promise.js
+++ promise.js
@@ -12,3 +12,22 @@ myPromise
     console.log(number);
   });
 
+const randomNumberJudge = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    const number = Math.random();
+    if(number > 0.5) {
+      resolve();
+    } else {
+      reject();
+    }
+    console.log('complete');
+  }, 1000);
+});
+
+randomNumberJudge.then(function() {
+  console.log('success');
+});
+
+randomNumberJudge.catch(function() {
+  console.log('fail');
+});
