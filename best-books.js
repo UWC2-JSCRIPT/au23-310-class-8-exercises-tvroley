cfe17f15d8b7611f0380813e7840cf69cd8a6ee5
Theodore Roley <tvroley@uw.edu>
2023-11-30 23:21:15 -0800

    started best book

--- best-books.js
+++ best-books.js
@@ -9,6 +9,40 @@ formEl.addEventListener('submit', function(e) {
   const year = yearEl.value;
   const month = monthEl.value;
   const date = dateEl.value;
+  const fullDate = `${year}-${month}-${date}`;
 
-  // Fetch bestselling books for date and add top 5 to page
+const BASE_URL = `https://api.nytimes.com/svc/books/v3/lists/${date}/hardcover-fiction.json`;
+
+const url = `${BASE_URL}?offset=20&api-key=${API_KEY}`;
+
+fetch(url)
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(responseJson) {
+    console.log(responseJson);
+    debugger;
+    let article = responseJson.response.docs[0];
+    console.log(article);
+    
+    const mainHeadline = article.headline.main;
+    document.getElementById('article-title').innerText = mainHeadline;
+
+    const snippet = article.snippet;
+    document.getElementById('article-snippet').innerText = snippet;
+
+    const articleLink = article.web_url;
+    const articleLinkEl = document.getElementById('article-link');
+    articleLinkEl.setAttribute('href', articleLink);
+    
+    const byLineEl = document.createElement('p');
+    const byLine = article.byline.original;
+    articleLinkEl.insertAdjacentElement('afterend', byLineEl);
+    byLineEl.innerText = byLine;
+
+    if (article.multimedia.length > 0) {
+      const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
+      document.getElementById('article-img').src = imgUrl;
+    }
+  });
 });
