e4f250290ea63a624cd15f795215e73704b8d192
Theodore Roley <tvroley@uw.edu>
2023-11-30 23:47:03 -0800

    made a list variable

--- best-books.js
+++ best-books.js
@@ -11,38 +11,40 @@ formEl.addEventListener('submit', function(e) {
   const date = dateEl.value;
   const fullDate = `${year}-${month}-${date}`;
 
-const BASE_URL = `https://api.nytimes.com/svc/books/v3/lists/${date}/hardcover-fiction.json`;
+  const list = 'hardcover-fiction';
 
-const url = `${BASE_URL}?offset=20&api-key=${API_KEY}`;
+  const BASE_URL = `https://api.nytimes.com/svc/books/v3/lists/${fullDate}/${list}.json`;
 
-fetch(url)
-  .then(function(data) {
-    return data.json();
-  })
-  .then(function(responseJson) {
-    console.log(responseJson);
-    debugger;
-    let article = responseJson.response.docs[0];
-    console.log(article);
-    
-    const mainHeadline = article.headline.main;
-    document.getElementById('article-title').innerText = mainHeadline;
+  const url = `${BASE_URL}?offset=20&api-key=${API_KEY}`;
+  debugger;
+  fetch(url)
+    .then(function(data) {
+      return data.json();
+    })
+    .then(function(responseJson) {
+      console.log(responseJson);
+      debugger;
+      let article = responseJson.response.docs[0];
+      console.log(article);
+      
+      const mainHeadline = article.headline.main;
+      document.getElementById('article-title').innerText = mainHeadline;
 
-    const snippet = article.snippet;
-    document.getElementById('article-snippet').innerText = snippet;
+      const snippet = article.snippet;
+      document.getElementById('article-snippet').innerText = snippet;
 
-    const articleLink = article.web_url;
-    const articleLinkEl = document.getElementById('article-link');
-    articleLinkEl.setAttribute('href', articleLink);
-    
-    const byLineEl = document.createElement('p');
-    const byLine = article.byline.original;
-    articleLinkEl.insertAdjacentElement('afterend', byLineEl);
-    byLineEl.innerText = byLine;
+      const articleLink = article.web_url;
+      const articleLinkEl = document.getElementById('article-link');
+      articleLinkEl.setAttribute('href', articleLink);
+      
+      const byLineEl = document.createElement('p');
+      const byLine = article.byline.original;
+      articleLinkEl.insertAdjacentElement('afterend', byLineEl);
+      byLineEl.innerText = byLine;
 
-    if (article.multimedia.length > 0) {
-      const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
-      document.getElementById('article-img').src = imgUrl;
-    }
+      if (article.multimedia.length > 0) {
+        const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
+        document.getElementById('article-img').src = imgUrl;
+      }
+    });
   });
-});
